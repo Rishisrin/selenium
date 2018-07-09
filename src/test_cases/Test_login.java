265f0d55dc22d05ab5a1a4a1d2cd75776b6255a7
Rishisrin <rishisree.nattey@gmail.com>
2018-07-09 10:24:27 +0530

    test

--- src/test_cases/Test_login.java
+++ src/test_cases/Test_login.java
@@ -9,7 +9,7 @@ public class Test_login extends browser_init{
 @Test
 public void log() throws InterruptedException{
 	login log= new login(driver);
-	log.login("rishisree425@gmail.com", "qwerty");
+	log.login("", "");
 }
 
 
